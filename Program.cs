d1b8e92d5e6afa69317153e5337a5ca6fca1d7f1
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 00:36:08 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Total=0;
+            string[] texto = Console.ReadLine().Split(' ');
+            int Cod = int.Parse(texto[0]), Qtd = int.Parse(texto[1]);
+
+            if( Cod == 1)
+            {
+                Total = 4.00 * Qtd;
+
+            }
+            else if( Cod == 2)
+            {
+                Total = 4.50 * Qtd;
+            }
+            else if (Cod == 3)
+            {
+                Total = 5.00 * Qtd;
+            }
+            else if (Cod == 4)
+            {
+                Total = 2.00 * Qtd;
+            }
+            else if (Cod == 5)
+            {
+                Total = 1.50 * Qtd;
+            }
+
+            Console.WriteLine("Total: R$ " + Total.ToString("#0.00"));
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
